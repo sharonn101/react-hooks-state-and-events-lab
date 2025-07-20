bcebe44e3d28c07de66b45478d7382213ff5a493
Sharon <sharonirungu2006@gmail.com>
2025-07-20 15:40:34 +0300

    adds events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
-  return (
-    <li className="">
+  const [addItem,setAddItem ] =useState()
+
+  function handleClick(){
+    setAddItem(!addItem);
+    }
+    return (
+    <li className={addItem ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addItem ? "remove" : "add"} onClick={handleClick}>
+      {addItem ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
